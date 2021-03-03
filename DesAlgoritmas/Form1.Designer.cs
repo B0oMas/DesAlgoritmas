96eb1b49a12b5b0ffa709074ea58ca6e72a8090f
Lukas Anužis <b0omasino@gmail.com>
2021-03-03 08:18:27 +0200

    Add project files.

---
+++ DesAlgoritmas/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace DesAlgoritmas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_text = new System.Windows.Forms.RichTextBox();
+            this.txt_key = new System.Windows.Forms.RichTextBox();
+            this.txt_result = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_encrypt = new System.Windows.Forms.Button();
+            this.btn_decrypt = new System.Windows.Forms.Button();
+            this.txt_decryptedText = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_upload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_text
+            // 
+            this.txt_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_text.Location = new System.Drawing.Point(16, 26);
+            this.txt_text.Name = "txt_text";
+            this.txt_text.Size = new System.Drawing.Size(265, 243);
+            this.txt_text.TabIndex = 0;
+            this.txt_text.Text = "";
+            // 
+            // txt_key
+            // 
+            this.txt_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_key.Location = new System.Drawing.Point(84, 318);
+            this.txt_key.Name = "txt_key";
+            this.txt_key.Size = new System.Drawing.Size(738, 29);
+            this.txt_key.TabIndex = 1;
+            this.txt_key.Text = "";
+            // 
+            // txt_result
+            // 
+            this.txt_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_result.Location = new System.Drawing.Point(287, 26);
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(265, 243);
+            this.txt_result.TabIndex = 2;
+            this.txt_result.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tekstas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Raktas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(287, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Užšifruotas tekstas";
+            // 
+            // btn_encrypt
+            // 
+            this.btn_encrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_encrypt.Location = new System.Drawing.Point(570, 365);
+            this.btn_encrypt.Name = "btn_encrypt";
+            this.btn_encrypt.Size = new System.Drawing.Size(120, 58);
+            this.btn_encrypt.TabIndex = 6;
+            this.btn_encrypt.Text = "Užšifruoti";
+            this.btn_encrypt.UseVisualStyleBackColor = true;
+            this.btn_encrypt.Click += new System.EventHandler(this.btn_encrypt_Click);
+            // 
+            // btn_decrypt
+            // 
+            this.btn_decrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_decrypt.Location = new System.Drawing.Point(701, 365);
+            this.btn_decrypt.Name = "btn_decrypt";
+            this.btn_decrypt.Size = new System.Drawing.Size(122, 58);
+            this.btn_decrypt.TabIndex = 7;
+            this.btn_decrypt.Text = "Dešifruoti";
+            this.btn_decrypt.UseVisualStyleBackColor = true;
+            this.btn_decrypt.Click += new System.EventHandler(this.btn_decrypt_Click);
+            // 
+            // txt_decryptedText
+            // 
+            this.txt_decryptedText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_decryptedText.Location = new System.Drawing.Point(558, 26);
+            this.txt_decryptedText.Name = "txt_decryptedText";
+            this.txt_decryptedText.Size = new System.Drawing.Size(265, 243);
+            this.txt_decryptedText.TabIndex = 8;
+            this.txt_decryptedText.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(566, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Dešifruotas tekstas";
+            // 
+            // btn_upload
+            // 
+            this.btn_upload.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_upload.Location = new System.Drawing.Point(26, 365);
+            this.btn_upload.Name = "btn_upload";
+            this.btn_upload.Size = new System.Drawing.Size(131, 58);
+            this.btn_upload.TabIndex = 10;
+            this.btn_upload.Text = "Failo ikelimas";
+            this.btn_upload.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 450);
+            this.Controls.Add(this.btn_upload);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_decryptedText);
+            this.Controls.Add(this.btn_decrypt);
+            this.Controls.Add(this.btn_encrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.txt_key);
+            this.Controls.Add(this.txt_text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txt_text;
+        private System.Windows.Forms.RichTextBox txt_key;
+        private System.Windows.Forms.RichTextBox txt_result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_encrypt;
+        private System.Windows.Forms.Button btn_decrypt;
+        private System.Windows.Forms.RichTextBox txt_decryptedText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_upload;
+    }
+}
+
---
+++ DesAlgoritmas/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesAlgoritmas
+{
+    public partial class Form1 : Form
+    {
+        DES des = new DES();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_encrypt_Click(object sender, EventArgs e)
+        {
+            txt_result.Text = des.EncryptionDES(txt_text.Text, txt_key.Text);
+        }
+
+        private void btn_decrypt_Click(object sender, EventArgs e)
+        {
+            txt_decryptedText.Text = des.DecryptionDES(txt_text.Text, txt_key.Text);
+        }
+    }
+}
